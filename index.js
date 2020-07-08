d9255821dba9ee6d288937b70b5b80e3a292dba2
afatoga <afatoga@gmail.com>
2020-07-08 17:44:24 +0200

    saving, validation contact items

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
+//const monitor = require("pg-monitor");
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -11,11 +12,27 @@ const jwt = require("jsonwebtoken");
 //const fs = require('fs');
 const bcrypt = require("bcrypt");
 const e = require("express");
+const { response } = require("express");
 
-const pgp = require("pg-promise")(/* options */);
-const db = pgp(
-  `postgres://${process.env.DB_NAME}:${process.env.DB_PW}@${process.env.DB_HOST}/${process.env.DB_NAME}`
-);
+const initOptions = {
+  // pg-promise initialization options;
+};
+
+const pgp = require("pg-promise")(initOptions);
+//monitor.attach(initOptions);
+
+// let query = pgp.as.format(query, values);
+// console.log(query);
+const cn = {
+  host: process.env.DB_HOST,
+  port: 5432,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  max: 30, // use up to 30 connections
+};
+
+const db = pgp(cn);
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -23,26 +40,24 @@ app.get("/", (req, res) => res.send("Hello World!"));
 
 // let's first add a /secret api endpoint that we will be protecting
 app.get("/secret", isAuthorized, (req, res) => {
-  res.json({ message: "THIS IS SUPER SECRET, DO NOT SHARE!" });
+  return res.json({ message: "THIS IS SUPER SECRET, DO NOT SHARE!" });
 });
 
 // and a /readme endpoint which will be open for the world to see
 app.get("/readme", (req, res) => {
-  res.json({ message: "This is open to the world!" });
+  return res.json({ message: "This is open to the world!" });
 });
 
 app.post("/api/register", (req, res) => {
   if (req.body["password"] === undefined || req.body["email"] === undefined) {
-    res.status(400).json({ error: "Email and password are mandatory" });
-    return;
+    return res.status(400).json({ error: "Email and password are mandatory" });
   }
 
   const email = req.body["email"].trim();
   const emailCheck = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
 
   if (!emailCheck.test(email)) {
-    res.status(400).json({ error: "Email is not valid" });
-    return;
+    return res.status(400).json({ error: "Email is not valid" });
   }
 
   bcrypt.hash(req.body["password"], 10, function (err, hash) {
@@ -55,23 +70,21 @@ app.post("/api/register", (req, res) => {
     )
       .then(function (data) {
         if (data.id !== undefined && !isNaN(data.id)) {
-          res.status(200).send("success");
+          return res.status(200).send("success");
         } else {
-          res.status(500).json({ error: "Connection problem" });
+          return res.status(500).json({ error: "Connection problem" });
         }
       })
       .catch(function (error) {
-        console.log("ERROR:", error);
-        res.status(500).json({ error: "Connection failed" });
+        //console.log("ERROR:", error);
+        return res.status(500).json({ error: "Connection failed" });
       });
   });
 });
 
 app.post("/api/login", (req, res) => {
-  db.one(
-    `SELECT password, role_id FROM afatoga.user WHERE email = $1`,
-    req.body.email
-  )
+  const email = req.body.email.toLowerCase();
+  db.one(`SELECT password, role_id FROM afatoga.user WHERE email = $1`, email)
     .then(function (data) {
       if (bcrypt.compareSync(req.body.password, data.password)) {
         let token = jwt.sign(
@@ -81,8 +94,8 @@ app.post("/api/login", (req, res) => {
         );
         res.status(200).json({
           token: token,
-          email: req.body.email,
-          /*name: req.body.email,*/ role_id: data.role_id,
+          email: email,
+          /*name: email,*/ role_id: data.role_id,
         });
       } else {
         res
@@ -101,25 +114,179 @@ app.post("/api/login", (req, res) => {
     });
 });
 
+app.get("/api/personList", isAuthorized, (req, res) => {
+  db.manyOrNone(
+    `SELECT person.id, names, surname, string_agg(org.name, ', ') orglist
+    FROM afatoga.person
+    LEFT OUTER JOIN afatoga.person_org ON person.id = person_org.person_id
+    LEFT OUTER JOIN afatoga.org ON person_org.org_id = org.id
+    GROUP BY person.id
+    LIMIT 10
+    `
+  )
+    .then(function (data) {
+      res.status(200).json(data);
+    })
+    .catch(function (error) {
+      //console.log('ERROR:', error);
+      // received = result.rows.length
+      res.status(500).json({ success: false, error: "Connection failed" });
+    });
+});
+
 app.get("/api/person", isAuthorized, (req, res) => {
-  if (!isNaN(req.query.userId)) {
-    db.one(
-      `SELECT id, names, surname, birthday_date FROM afatoga.person WHERE id = $1`,
-      req.query.userId
-    )
-      .then(function (data) {
-        if (data.id !== undefined && !isNaN(data.id)) {
-          res.status(200).json({ success: true, data: data });
+  let profileContact = {};
+
+  db.task(async (t) => {
+    const personProfile = await t.one(
+      `
+      SELECT person.*
+      FROM afatoga.person
+      WHERE person.id = $1
+      `,
+      req.query.personId
+    );
+
+    if (personProfile) {
+      const contact_person = await t.manyOrNone(
+        `
+        SELECT person_id, contact_type_id, contact_id, is_active, validation_request_id, email.email, phone_number.phone_number
+          FROM afatoga.contact_person
+          LEFT JOIN afatoga.email 
+            ON email.id = contact_id 
+            AND contact_type_id = $(type_email)
+          LEFT JOIN afatoga.phone_number 
+            ON phone_number.id = contact_id 
+            AND contact_type_id = $(type_phone_number)
+          WHERE is_active = TRUE
+          AND person_id = $(person_id)`,
+        { type_email: 1, type_phone_number: 2, person_id: req.query.personId }
+      );
+
+      profileContact.contact_person = contact_person ? contact_person : null;
+
+      const person_org = await t.manyOrNone(
+        `
+          SELECT id, person_id, org_id
+          FROM afatoga.person_org
+          WHERE is_active = TRUE
+          AND person_id = $1
+        `,
+        req.query.personId
+      );
+
+      let orgIds = [];
+
+      for (
+        let i = 0, person_org_items = person_org.length;
+        i < person_org_items;
+        i++
+      ) {
+        orgIds.push(person_org[i].id);
+      }
+
+      if (orgIds.length) {
+        const contact_person_org = await t.manyOrNone(
+          `
+          SELECT person_org_id, contact_type_id, contact_id, email.email, phone_number.phone_number
+          FROM afatoga.contact_person_org
+          LEFT JOIN afatoga.email ON email.id = contact_id AND contact_type_id = $(type_email)
+          LEFT JOIN afatoga.phone_number ON phone_number.id = contact_id AND contact_type_id = $(type_phone_number)
+          WHERE is_active = TRUE
+          AND person_org_id IN ($(orgIds:list))
+        `,
+          { type_email: 1, type_phone_number: 2, orgIds: orgIds }
+        );
+
+        profileContact.contact_person_org = contact_person_org;
+      }
+    }
+
+    const profileTotal = {
+      profile: personProfile,
+      contact: profileContact,
+    };
+
+    return profileTotal;
+  })
+    .then(function (profileTotal) {
+      res.status(200).json({ success: true, data: profileTotal });
+      /*if (data.id !== undefined && !isNaN(data.id)) {
+        res.status(200).json({ success: true, data: data });
+      }*/
+    })
+    .catch(function (error) {
+      //console.log(error);
+      if (error.received === 0) {
+        res.status(400).json({ success: false, error: "Person not found" });
+      }
+    });
+});
+
+app.post("/api/person/contact_person", isAuthorized, (req, res) => {
+  const reqData = req.body;
+  let returningData = {};
+
+  db.task(async (t) => {
+    if (reqData.contact_item_id === undefined) {
+      const response = await t.oneOrNone(
+        `INSERT INTO afatoga.$(contact_type_name~) 
+        ($(contact_type_name~)) 
+        VALUES 
+        ($(value))
+        RETURNING afatoga.$(contact_type_name~).id`,
+        {
+          contact_type_name: reqData.contact_type_name,
+          value: reqData.contact_value,
         }
-      })
-      .catch(function (error) {
-        if (error.received === 0) {
-          res.status(400).json({ success: false, error: "Person not found" });
+      );
+
+      const contact_type_id = reqData.contact_type_name === "email" ? 1 : 2;
+      if (response.id > 0) {
+        returningData = {
+          person_id: reqData.person_id,
+          contact_type_id: contact_type_id,
+          contact_id: response.id,
+        };
+
+        await t.none(
+          `INSERT INTO afatoga.contact_person 
+          ("person_id", "contact_type_id", "contact_id") 
+          VALUES 
+          ($(person_id), $(contact_type_id), $(contact_id))`,
+          returningData
+        );
+      } else {
+        res.status(400).json({ success: false, error: "Unable to save item" });
+      }
+    } else {
+      const response = await t.oneOrNone(
+        `INSERT INTO afatoga.validation_log ("relation_table_name","item_id") VALUES ($(tableName), $(itemId)) RETURNING afatoga.validation_log.id`,
+        {
+          tableName: "contact_person",
+          itemId: reqData.contact_item_id,
         }
-      });
-  } else {
-    res.status(500).json({ error: "Connection failed" });
-  }
+      );
+
+      returningData = { validation_request_id: reponse.id };
+
+      await t.none(
+        `UPDATE afatoga.contact_person SET validation_request_id = $(validation_request_id) WHERE person_id = $(person_id) AND contact_id = $(contact_item_id)`,
+        {
+          validation_request_id: response.id,
+          person_id: reqData.person_id,
+          contact_item_id: reqData.contact_item_id,
+        }
+      );
+    }
+  })
+    .then(() => {
+      res.status(200).json({ success: true, data: returningData });
+    })
+    .catch((error) => {
+      //console.log("ERROR:", error);
+      res.status(500).json({ error: "Connection failed" });
+    });
 });
 
 app.post("/api/person", isAuthorized, (req, res) => {
@@ -179,16 +346,10 @@ app.post("/api/person", isAuthorized, (req, res) => {
   } else {
     db.task(async (t) => {
       //returns two rows...
-      const currentPersonData = await t.one(
-        `SELECT person.*, email.email, phone_number.phone_number
-      FROM afatoga.person
-      LEFT JOIN afatoga.contact_person ON person.id = contact_person.person_id
-      LEFT JOIN afatoga.email ON contact_person.contact_id = email.id
-      LEFT JOIN afatoga.phone_number ON contact_person.contact_id = phone_number.id
-      WHERE person.id = $1 
-      AND contact_person.is_active = true`,
-        inputData.id
-      );
+      // const currentPersonData = await t.one(
+      //   ``,
+      //   inputData.id
+      // );
 
       await t.none(
         `UPDATE afatoga.person
@@ -212,8 +373,11 @@ app.post("/api/person", isAuthorized, (req, res) => {
         );
 
         const data = await t.one(
-          `INSERT INTO afatoga.email ("email") VALUES ($1) RETURNING afatoga.email.id`,
-          inputData.email
+          `INSERT INTO afatoga.$(contact_type_name~) ($(contact_type_name~)) VALUES ($(value)) RETURNING afatoga.$(contact_type_name~).id`,
+          {
+            contact_type_name: inputData.contact_type_name,
+            value: inputData.email,
+          }
         );
         if (!isNaN(data.id)) {
           await t.none(
@@ -249,13 +413,13 @@ app.post("/api/person", isAuthorized, (req, res) => {
         }
       }
     })
-    .then(() => {
-      res.status(200).json({ success: true, data: inputData });
-    })
-    .catch((error) => {
-      console.log("ERROR:", error);
-      res.status(500).json({ error: "Connection failed" });
-    });
+      .then(() => {
+        res.status(200).json({ success: true, data: inputData });
+      })
+      .catch((error) => {
+        console.log("ERROR:", error);
+        res.status(500).json({ error: "Connection failed" });
+      });
   }
 });
 
